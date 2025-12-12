d960c59e047304a5c87d5c1b08a4f6b9f086556b
shunsuke-ids <shunsuke.anzai@is.ids.osaka-u.ac.jp>
2025-12-12 17:45:27 +0900

    ベクトルの長さの正則化

--- pytorch/losses.py
+++ pytorch/losses.py
@@ -29,10 +29,13 @@ class SoftmaxVectorLoss(nn.Module): # nn.Moduleクラスを継承
             ロス
         """
         y_pred_softmax = F.softmax(y_pred, dim=1)
-        y_true_onehot = F.one_hot(y_true.long(), num_classes=self.num_classes).float()
+        y_true_onehot = F.one_hot(y_true.long(), num_classes=self.num_classes).float().to(y_pred.device)
 
         pred_vector = torch.matmul(y_pred_softmax, self.class_coords)
         true_vector = torch.matmul(y_true_onehot, self.class_coords)
+
+        pred_vector = F.normalize(pred_vector, p=2, dim=-1)
+        true_vector = F.normalize(true_vector, p=2, dim=-1)
 
         dot_product = torch.sum(pred_vector * true_vector, dim=-1)
         loss = 1.0 - dot_product
